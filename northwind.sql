d2809a751ebdc7c374eb1e30c0cc68d9b512bb79
yaakov hatam <kobieshka@gmail.com>
2017-06-28 19:24:50 +0300

    solution

---
+++ northwind.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 28, 2017 at 06:24 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `northwind`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `l34_cutomers`
+--
+
+CREATE TABLE `l34_cutomers` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `l34_products`
+--
+
+CREATE TABLE `l34_products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `l34_productscol` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `l34_purchase`
+--
+
+CREATE TABLE `l34_purchase` (
+  `social_security` int(11) NOT NULL,
+  `salesman_social_security` int(11) DEFAULT NULL,
+  `purchase_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `date_of_purchase` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `l34_cutomers`
+--
+ALTER TABLE `l34_cutomers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `l34_products`
+--
+ALTER TABLE `l34_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `l34_purchase`
+--
+ALTER TABLE `l34_purchase`
+  ADD PRIMARY KEY (`social_security`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
